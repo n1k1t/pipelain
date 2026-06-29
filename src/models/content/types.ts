5a4f5c0e678fa76afe67eaf77ef9bdbc714d958e
n1k1t <n1k1t.developer@gmail.com>
2026-06-29 20:32:42 +0300

    fix: json2md typings

--- src/models/content/types.ts
+++ src/models/content/types.ts
@@ -1,4 +1,17 @@
-import { DataObject } from 'json2md';
+interface IImgInput {
+  title: string;
+  source: string;
+}
+
+interface ICodeInput {
+  language?: string | undefined;
+  content: string | string[];
+}
+
+interface ITableInput {
+  headers: string[];
+  rows: Array<{ [column: string]: string }> | string[][];
+}
 
 export interface IJson2MdDataObjectExtension {
   plain: string;
@@ -11,4 +24,19 @@ export interface IJson2MdDataObjectExtension {
   };
 }
 
-export interface IJsonContent extends DataObject, Partial<IJson2MdDataObjectExtension> {}
+export interface IJsonContent extends Partial<IJson2MdDataObjectExtension> {
+  blockquote?: string | string[];
+  h1?: string | string[];
+  h2?: string | string[];
+  h3?: string | string[];
+  h4?: string | string[];
+  h5?: string | string[];
+  h6?: string | string[];
+  ul?: string[];
+  ol?: string[];
+  p?: string | string[];
+
+  table?: ITableInput;
+  code?: ICodeInput;
+  img?: IImgInput | IImgInput[];
+}
